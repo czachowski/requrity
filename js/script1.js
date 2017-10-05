e03ae260760ecc5293867b67bff45a799f6b1ed9
Szymon <32205986+czachowski@users.noreply.github.com>
2017-10-05 17:28:47 +0200

    commit

--- js/script1.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$("logo.biglogo img").hide();
-	$("div.start").hide();
-	$("div.start.btn1").hide();
-	$('.plywak').hide();
-    $("logo.biglogo img").fadeIn(2000);
-    $("div.start").fadeIn(2000);
-    $("div.start.btn1").fadeIn(2000);
-});
--- js/script3.js
+++
@@ -1,8 +0,0 @@
-function update() {
-  if ($(window).scrollTop() > 900) {
-    $('.plywak').fadeIn(500);
-  } else {
-    $('.plywak').fadeOut(300);
-  }
-}
-setInterval(update, 1);
